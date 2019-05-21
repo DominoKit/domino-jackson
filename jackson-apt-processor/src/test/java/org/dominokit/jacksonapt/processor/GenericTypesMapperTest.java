6377c70bb3e8e0669cbde43229ae0db2b07de976
Teodor Naydenov <teodor.naydenov@honeywell.com>
2019-05-21 16:19:17 +0300

    Remove some unused imports

--- jackson-apt-processor/src/test/java/org/dominokit/jacksonapt/processor/GenericTypesMapperTest.java
+++ jackson-apt-processor/src/test/java/org/dominokit/jacksonapt/processor/GenericTypesMapperTest.java
@@ -1,10 +1,7 @@
 package org.dominokit.jacksonapt.processor;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
