54ee812e3eb04b54343886913041d2b421817bfb
Ahmad bawaneh <akabme@gmail.com>
2021-02-18 14:13:09 +0200

    prepare for release

--- jackson-apt-processor/src/main/java/org/dominokit/jacksonapt/processor/MappersChainBuilder.java
+++ jackson-apt-processor/src/main/java/org/dominokit/jacksonapt/processor/MappersChainBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Ahmad Bawaneh
+ * Copyright © 2019 Dominokit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
