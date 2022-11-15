43064e01db7f4bf4d53d13f6937ba8fb0e76fc1e
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2022-11-15 15:16:17 +0300

    fix #61 domino-jackson does not work with j2cl-maven-plugin version 0.20 due to exception with specific for loop syntax

--- domino-jackson/src/main/java/org/dominokit/jackson/deser/collection/BaseCollectionJsonDeserializer.java
+++ domino-jackson/src/main/java/org/dominokit/jackson/deser/collection/BaseCollectionJsonDeserializer.java
@@ -95,9 +95,7 @@ public abstract class BaseCollectionJsonDeserializer<C extends Collection<T>, T>
   public void setBackReference(
       String referenceName, Object reference, C value, JsonDeserializationContext ctx) {
     if (null != value && !value.isEmpty()) {
-      for (T val : value) {
-        deserializer.setBackReference(referenceName, reference, val, ctx);
-      }
+      value.forEach(val -> deserializer.setBackReference(referenceName, reference, val, ctx));
     }
   }
 }
